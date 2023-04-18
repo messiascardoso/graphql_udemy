cd96d3fced34859329e5dda62be5a93b7ea2bedb
Messias Cardoso <messiascardoso@gmail.com>
2023-04-17 23:52:13 -0300

    Criado query com mock 2

--- shema-query/index.js
+++ shema-query/index.js
@@ -1,11 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const usuarios = [
+  {
+    id: 1,
+    nome: 'usuario 1',
+    email: 'usuario@gmail.com',
+    idade: 19
+  },
+  {
+    id: 2,
+    nome: 'usuario 2',
+    email: 'usuario2@gmail.com',
+    idade: 29
+  },
+  {
+    id: 3,
+    nome: 'usuario 3',
+    email: 'usuario3@gmail.com',
+    idade: 39
+  },
+]
+
 // Pontos de entrada da sua API! / Consultas
 // ! vira um campo obrigatorio
 
 const typeDefs = gql`
   # Cria um novo tipo de dado (Existe somente 5 tipos: Int, String, float, Boolean, ID )
   scalar Date
+
+  type Produto {
+    nome: String
+    preco: Float!
+    desconto: Float
+    precoComDesconto: Float
+  }
+
 
   type Usuario {
     id: ID!
@@ -20,12 +49,25 @@ const typeDefs = gql`
     ola: String
     horaAtual: Date
     usuarioLogado: Usuario
+    produtoEmDestaque: Produto
+    numerosMegaSena: [Int]!
+    usuarios: [Usuario]!
   }
 `
 
 //Conjunto de funcoes para resolver dados 
 // Para para query precisa de um revolver
 const resolvers = {
+
+    Produto:  {
+      precoComDesconto(produto) {
+        if (produto.desconto) {
+          return produto.preco * (1 - produto.desconto)
+        }else {
+          return produto.preco
+        }
+      }
+    },
     // Foi criado para resolver o atributo `salario_real` que é diferente do type
     Usuario: {
       salario(usuario) {
@@ -51,7 +93,25 @@ const resolvers = {
           idade: 30,
           salario_real: 1000,
         }
+      },
+      produtoEmDestaque() {
+        return {
+          nome: 'Notebook gamer',
+          preco: 500,
+          desconto: 0.20
+        }
+      },
+      numerosMegaSena() {
+        const crescente = (a, b) => a- b;
+        return Array(6).fill(0)
+        .map(n => parseInt(Math.random() * 60 + 1 ))
+        .sort(crescente)
+      },
+      usuarios() {
+        return usuarios
       }
+
+      
    }
 }
 
